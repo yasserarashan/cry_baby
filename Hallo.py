c888e22a05ab7f33b48a9a1708d2b3dc6f0baa02
yasserarashan <aaaw737625336y@gmail.com>
2025-02-21 04:31:31 +0300

    Update Hallo.py

--- Hallo.py
+++ Hallo.py
@@ -11,7 +11,7 @@ from pydub import AudioSegment
 from pydub.exceptions import CouldntDecodeError
 
 # إعدادات TensorFlow
-os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
 
 # تهيئة التطبيق
 app = FastAPI()
